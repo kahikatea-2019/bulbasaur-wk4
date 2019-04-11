e2fc80442bbea6c85bdce8f000db507e1c4d7849
Philip Ji <phil12p345@gmail.com>
2019-04-12 11:36:00 +1200

    display letter

--- client/components/PixelLetter.jsx
+++ client/components/PixelLetter.jsx
@@ -2,20 +2,25 @@ import React from 'react'
 
 import Pixel from './Pixel'
 
-const pixelArray = Array.from({ length: 5 }, () => {
-  return Array.from({ length: 5 }, () => <Pixel />)
-})
+import letterArrays from '../../lettersData'
 
 class PixelLetter extends React.Component {
-  
   componentDidMount () {
-    console.log(pixelArray)
+    console.log(this.pixelArray)
   }
+
+  pixelArray = letterArrays.A.map((arr, i) => {
+    return arr.map((pixel, j) => {
+      let color = '#adff9e'
+      if (pixel) { color = '#f82035' }
+      return <Pixel key={j} style = {{ backgroundColor: color }}/>
+    })
+  })
 
   render () {
     return (
       <div className="pixel-name">
-        {pixelArray}
+        {this.pixelArray}
       </div>
     )
   }
